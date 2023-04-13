657a6de775fad006808029689cfcbc0e549e52a5
Srihari Krishnaswamy <srihari@uw.edu>
2023-04-12 18:16:32 -0700

    nav bar set up

--- app/index.js
+++ app/index.js
@@ -8,9 +8,29 @@ const Home = () => {
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}> 
             <Stack.Screen 
-            options={{
-                headerStyle: {backgroundColor: COLORS.lightWhite}
+                options={{
+                    headerStyle: {backgroundColor: COLORS.lightWhite}, 
+                    headerShadowVisible: false, 
+                    headerLeft: () => {
+                        return (<ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/> )
+                    }, 
+                    headerRight: () => {
+                        return (<ScreenHeaderBtn iconUrl={images.profile} dimension="100%"/>)
+                    },
+                    headerTitle: ""
             }}/>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex: 1, 
+                    padding: SIZES.medium
+                }}>
+                    <Welcome
+
+                    />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import {useState} from 'react'
+import { 
+  View, 
+  Text, 
+  TextInput,
+  TouchableOpacity, 
+  Image,
+  FlatList
+ } from 'react-native'
+import {useRouter} from 'expo-router'
 import styles from './welcome.style'
-
+import {icons, SIZES} from '../../../constants'
 const Welcome = () => {
   return (
     <View>
