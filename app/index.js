98eed10349ba85bf773e81fcaf4362b7f0aa2cd1
Srihari Krishnaswamy <srihari@uw.edu>
2023-04-12 23:49:50 -0700

    Search working

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import {COLORS, icons, images, SIZES} from '../constants'
 import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components'
 const Home = () => {
     const router = useRouter();
+    const [searchTerm, setSearchTerm] = useState("")
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}> 
             <Stack.Screen 
@@ -25,7 +26,13 @@ const Home = () => {
                     padding: SIZES.medium
                 }}>
                     <Welcome
-
+                        searchTerm={searchTerm}
+                        setSearchTerm={setSearchTerm}
+                        handleClick={() => {
+                            if(searchTerm) {
+                                router.push('/search/' + searchTerm )
+                            }
+                        }}
                     />
                     <Popularjobs />
                     <Nearbyjobs />
